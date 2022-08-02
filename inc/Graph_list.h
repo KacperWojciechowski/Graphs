6c06eb997a33585b85723e9e43ee6b93fbe5e346
Kacper Wojciechowski <k.woj.coding@gmail.com>
2022-08-02 18:20:07 +0200

    moved Graph_matrix.cpp file into right directory, created Graph_list header file

--- inc/Graph_list.h
+++ inc/Graph_list.h
@@ -1,12 +1,22 @@
 #pragma once
 
-#include <cstdint>
-#include 
+#include <string>
+#include <map>
 
+#include "Graph_matrix.h"
 #include "Graph.h"
 
 namespace Graph
 {
+	// forward declaration
+	class Matrix;
+
+	/**
+	 * \brief Class representing a graph in the adjacency list form.
+	 * 
+	 * The representation is based on the STL list and vector classes. It does provide a
+	 * standard graph interface stated by the Graph virtual class.
+	 */
 	class List
 	{
 

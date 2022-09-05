aad6ee89d79ff4200f0df42fff6ec4d9da63e9f4
Kacper Wojciechowski <k.woj.coding@gmail.com>
2022-09-06 00:14:45 +0200

    added includes

--- inc/Graphs.h
+++ inc/Graphs.h
@@ -1,3 +1,14 @@
 #pragma once
 
-#include 
+/**
+ * \file A common include to access the whole library at once.
+ */
+
+// include Graph namespace elements
+#include "Graph_list.h"
+#include "Graph_matrix.h"
+
+// include Data namespace elements
+#include "PixelMap.h"
+#include "Generator.h"
+ 
